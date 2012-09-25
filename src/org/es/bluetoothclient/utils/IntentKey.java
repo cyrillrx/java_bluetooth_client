1b2f83bd581e3c777414a4886866c4e18a499c80
Cyril Leroux <cyril0leroux@gmail.com>
2012-09-26 00:26:25 +0200

    Implemented connection service

--- src/org/es/bluetoothclient/utils/IntentKey.java
+++ src/org/es/bluetoothclient/utils/IntentKey.java
@@ -8,5 +8,16 @@ package org.es.bluetoothclient.utils;
 public class IntentKey {
 
 	/** Device Address key. */
-	public static String EXTRA_DEVICE_ADDRESS = "device_address";
+	public static String EXTRA_DEVICE_ADDRESS	= "device_address";
+	// Key names received from the BluetoothChatService Handler
+	public static final String DEVICE_NAME		= "device_name";
+	public static final String TOAST			= "toast";
+
+
+	// Message types sent from the BluetoothChatService Handler
+	public static final int MESSAGE_STATE_CHANGE	= 1;
+	public static final int MESSAGE_READ			= 2;
+	public static final int MESSAGE_WRITE			= 3;
+	public static final int MESSAGE_DEVICE_NAME		= 4;
+	public static final int MESSAGE_TOAST			= 5;
 }
